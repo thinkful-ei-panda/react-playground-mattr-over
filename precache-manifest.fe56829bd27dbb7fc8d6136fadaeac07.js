ce43fd6bf0bbd27394b9cc5b046aa01f2aa7548b
Matthew Rougely <matthewrougelysr@gmail.com>
2020-06-19 13:57:58 -0700

    Updates

---
+++ precache-manifest.fe56829bd27dbb7fc8d6136fadaeac07.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "133660c26ae3b958e4961d2b21752166",
+    "url": "/react-playground-mattr-over/index.html"
+  },
+  {
+    "revision": "dfa96d5cae7860c19a5d",
+    "url": "/react-playground-mattr-over/static/css/main.f285c646.chunk.css"
+  },
+  {
+    "revision": "1f5f282cd308914813bc",
+    "url": "/react-playground-mattr-over/static/js/2.f7d0f4ad.chunk.js"
+  },
+  {
+    "revision": "d6015918a6681022fb6b15a0ac91888f",
+    "url": "/react-playground-mattr-over/static/js/2.f7d0f4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfa96d5cae7860c19a5d",
+    "url": "/react-playground-mattr-over/static/js/main.4c5472aa.chunk.js"
+  },
+  {
+    "revision": "751fb2c86282f58ce696",
+    "url": "/react-playground-mattr-over/static/js/runtime-main.6fa628e4.js"
+  }
+]);
